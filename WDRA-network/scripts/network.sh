68b0691631c3e9f7cfbaa9b21c03b3921affc9a9
durgakiran <kiranfet@gmail.com>
2022-09-24 09:34:17 +0530

    Add docker compose files

--- WDRA-network/scripts/network.sh
+++ WDRA-network/scripts/network.sh
@@ -3,18 +3,41 @@
 ROOTDIR=$(cd "$(dirname "$0")" && pwd)
 echo $ROOTDIR
 export PATH=${ROOTDIR}/../../bin:${PWD}/../../bin:$PATH
-export FABRIC_CFG_PATH=${PWD}/config
+export FABRIC_CFG_PATH=${PWD}/../config
 export VERBOSE=false
 
 # import utils functions
 . ./utils.sh
+
+function generateGenisisBlockForOrderer() {
+    which configtxgen
+    if [ "$?" -ne 0 ]; then
+        fatalln "configtxgen tool not found. exiting"
+    fi
+    infoln "Generating genesis block for orderer"
+
+    mkdir -p ../output/ ../output/channel-artifacts
+
+    configtxgen -profile SixOrgOrdererGenesis -channelID $CHANNEL_NAME -outputBlock ../output/channel-artifacts/genesis.block
+    if [ "$?" -ne 0 ]; then
+        echo "Failed to generate channel genesis block"
+    fi
+}
+
+function createChannel() {
+    which configtxgen
+    if [ "$?" -ne 0 ]; then
+        fatalln "configtxgen tool not found. exiting"
+    fi
+    infoln "Generating genesis block for orderer"
+
+}
 
 function createOrgs() {
     # remove existing organizations if already present
     if [ -d "organizations/peerOrganizations" ]; then
         rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
     fi
-
 
     # generate crypto material for organizations
     # here we are using cryptogen only
@@ -31,7 +54,7 @@ function createOrgs() {
         res=$?
         { set +x; } 2>/dev/null
         if [ $res -ne 0 ]; then
-          fatalln "Failed to generate certificates..."
+            fatalln "Failed to generate certificates..."
         fi
 
         infoln "Creating Collateral peer Identities"
@@ -40,7 +63,7 @@ function createOrgs() {
         res=$?
         { set +x; } 2>/dev/null
         if [ $res -ne 0 ]; then
-          fatalln "Failed to generate certificates..."
+            fatalln "Failed to generate certificates..."
         fi
 
         infoln "Creating insurance peer Identities"
@@ -49,7 +72,7 @@ function createOrgs() {
         res=$?
         { set +x; } 2>/dev/null
         if [ $res -ne 0 ]; then
-          fatalln "Failed to generate certificates..."
+            fatalln "Failed to generate certificates..."
         fi
 
         infoln "Creating transport peer Identities"
@@ -58,7 +81,7 @@ function createOrgs() {
         res=$?
         { set +x; } 2>/dev/null
         if [ $res -ne 0 ]; then
-          fatalln "Failed to generate certificates..."
+            fatalln "Failed to generate certificates..."
         fi
 
         infoln "Creating warehouse peer Identities"
@@ -67,10 +90,8 @@ function createOrgs() {
         res=$?
         { set +x; } 2>/dev/null
         if [ $res -ne 0 ]; then
-          fatalln "Failed to generate certificates..."
+            fatalln "Failed to generate certificates..."
         fi
-
-
 
         infoln "Creating Orderer Org Identities"
 
@@ -86,23 +107,24 @@ function createOrgs() {
 
 function networkUp() {
     createOrgs
+    generateGenisisBlockForOrderer
 }
 
 # Using crpto vs CA. default is cryptogen
 CRYPTO="cryptogen"
-
+# Default channel that get's created and all organizations would be part of this
+CHANNEL_NAME="mainChannel"
 
 # Parse commandline args
 
 ## Parse mode
-if [[ $# -lt 1 ]] ; then
+if [[ $# -lt 1 ]]; then
     printHelp
     exit 0
 else
     MODE=$1
     shift
 fi
-
 
 # Are we generating crypto material with this command?
 if [ ! -d "organizations/peerOrganizations" ]; then
@@ -111,9 +133,8 @@ else
     CRYPTO_MODE=""
 fi
 
-
 # Determine mode of operation and printing out what we asked for
 if [ "$MODE" == "up" ]; then
-  infoln "Starting nodes with CLI timeout of '${MAX_RETRY}' tries and CLI delay of '${CLI_DELAY}' seconds and using database '${DATABASE}' ${CRYPTO_MODE}"
-  networkUp
+    infoln "Starting nodes with CLI timeout of '${MAX_RETRY}' tries and CLI delay of '${CLI_DELAY}' seconds and using database '${DATABASE}' ${CRYPTO_MODE}"
+    networkUp
 fi
